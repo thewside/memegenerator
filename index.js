ff16648213417b5457fd45d7eed6ad7b55f43867
Roman <ufanet.123@yandex.ru>
2023-02-27 19:50:53 +0500

    fix

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 require("dotenv").config();
-const { Client, Events, GatewayIntentBits, REST, Routes, Partials } = require('discord.js')
+const { Client, Events, GatewayIntentBits, Partials } = require('discord.js')
 const token = process.env.CLIENT_TOKEN
 const { random } = require("./utils/random.js")
 const { JSDOM } = require('jsdom')
 const D3Node = require('d3-node')
-const d3n = new D3Node()   
-const d3 = d3n.d3
-const dom = new JSDOM(`<!DOCTYPE html><body></body>`)
 const { convert } = require('convert-svg-to-png')
 const querystring = require("querystring")
 
@@ -28,196 +25,210 @@ client.once(Events.ClientReady, c => {
   console.log(`Ready! Logged in as ${c.user.tag}`)
 })
 
-const newBreak = (svg, text, height, fontSize, wordInterval) => {
-  svg.append("text")
-      .attr("x", "50%")
-      .style("fill", "white")
-      .attr("text-anchor", "middle")
-      .attr("lengthAdjust", "spacingAndGlyphs")
-      .attr("y", height)
-      .attr("font-size", `${fontSize}%`)
-      .attr("textLength", wordInterval)
-      .attr("font-family", "Times New Roman") //Times New Roman
-      .text(text)
+
+const sliceTextToParts = (text) => {
+  console.log(text, "slicetextparts")
+  let textContainer = []
+  let letters = ""
+  for (let i = 0; i < text.length; i++) {
+    letters += text[i]
+    if (letters.length > 30 && text[i] === " " || letters.length > 40) {
+      textContainer.push(letters.slice(0, 40).trim())
+      letters = ""
+    } else if (i === text.length - 1) {
+      textContainer.push(letters.trim())
+    }
+  }
+  return textContainer
 }
 
-const reactionOnEmoji = async (reaction) => {
-  if(reaction.emoji.reaction._emoji.name !== params.requiredEmoji) return
-  const message = await reaction.message.fetch(true)
-  if(!message.content) return
-  if(message.content === "") return
-  const createURL = (messageContent) => {
-    const parseURL = new URL(params.requiredURL)
-    parseURL.pathname = params.pathname
-    //querystring.stringify()
-    parseURL.search = querystring.stringify({q:`=${messageContent}`}) + params.searchOptions
-    return parseURL.href
-  }
+const createUrlFromMessage = (messageContent) => {
+  const parseURL = new URL(params.requiredURL)
+  parseURL.pathname = params.pathname
+  parseURL.search = querystring.stringify({ q: `=${messageContent}` }) + params.searchOptions
+  return parseURL.href
+}
 
-  const fetchImage = async (url) => {
-    let newUrl = null
-    let count = 0
-    try {
-      const response = await fetch(url)
-      const result = await response.json()
-      const getUrl = () => result.results[random(0, result.results.length)]?.img_src
-      newUrl = getUrl()
+const createUrlImage = (container) => container[random(0, container.length)]?.img_src
+const getUrlImage = async (url) => {
+  let newUrl = null
+  let count = 0
+  let maxCount = 5
+  try {
+    const response = await fetch(url)
+    const result = await response.json()
+    newUrl = createUrlImage(result.results)
+    console.log(newUrl, "url created")
 
-      console.log(newUrl, "url created")
-
-      if(!newUrl) {
-        console.log(newUrl, "bad url")
-        count++
-        console.log(count, " retries")
-        newUrl = getUrl()
-      } 
-    
-      if(count >= 3) {
-        console.log(count, " retries and ...stop ")
-        return null
-      }
-      console.log("check")
-      const checkImage = async() => {
-        try {
-          const imageResult = await fetch(newUrl)
-          if(imageResult.headers.get("content-type").includes("image")) {
-            return newUrl
-          } else {
-            return null
-          }
-        } catch (error) {
-          return null
-        }
-      }
-      return checkImage()
-
-    } catch (error) {
-      console.log(error)
-      console.log("fetch error")
+    if (!newUrl) {
+      console.log(newUrl, "bad url")
+      count++
+      console.log(count, " retries")
+      newUrl = createUrlImage()
     }
-  }
 
-  const createSvg = async (url) => {
-  
-    const sliceTextToParts = () => {
-      const text = message.content
-      let textContainer = []
-      let letters = ""
-      for (let i = 0; i < text.length; i++) {
-        letters += text[i]
-        if (letters.length > 30 && text[i] === " " || letters.length > 40) {
-          textContainer.push(letters.slice(0, 40).trim())
-          letters = ""
-        } else if (i === text.length - 1) {
-          textContainer.push(letters.trim())
-        }
-      }
-      return textContainer
+    if (count >= maxCount) {
+      console.log(count, " retries and ...stop ")
+      return null
     }
-    const content = sliceTextToParts()
-  
-    const body = d3.select(dom.window.document.querySelector("body"))
-    const svg = body.append('svg')
-        .attr('width', params.width)
-        .attr('height', params.height)
-        .attr('xmlns', 'http://www.w3.org/2000/svg')
-        .style("fill", "black")
-  
-    svg.append("rect")
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", params.width)
-        .attr("height", params.height)
-        .style("fill", "black")
-  
-    svg.append("rect")
-        .attr("x", 50)
-        .attr("y", 50)
-        .attr("width", params.width - 100)
-        .attr("height", params.height - 200)
+    console.log("check")
+    return newUrl
+
+  } catch (error) {
+    console.log(error)
+    console.log("fetch error")
+    return null
+  }
+}
+
+const validateImage = async (newUrl) => {
+  let status = null
+  try {
+    const imageResult = await fetch(newUrl)
+    console.log(imageResult.headers.get("content-type"))
+    if (imageResult.headers.get("content-type").includes("image")) {
+      status = newUrl
+    }
+  } catch (error) {
+    console.log("error validation image")
+  }
+  return status
+}
+
+const createSvg = async (body, url, message) => {
+  const content = sliceTextToParts(message)
+  const svg = body.append('svg')
+    .attr('width', params.width)
+    .attr('height', params.height)
+    .attr('xmlns', 'http://www.w3.org/2000/svg')
+    .style("fill", "black")
+
+  svg.append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", params.width)
+    .attr("height", params.height)
+    .style("fill", "black")
+
+  svg.append("rect")
+    .attr("x", 50)
+    .attr("y", 50)
+    .attr("width", params.width - 100)
+    .attr("height", params.height - 200)
+    .style("fill", "white")
+
+  svg.append("rect")
+    .attr("x", 52)
+    .attr("y", 52)
+    .attr("width", params.width - 104)
+    .attr("height", params.height - 204)
+    .style("fill", "black")
+
+  svg.append("image")
+    .attr("x", "52")
+    .attr("y", "52")
+    .attr('xlink:href', url)
+    .attr('width', "89.8%")
+    .attr('height', "73.4%")
+
+  let heightText = 670
+  let fontSize = 300
+  let wordInterval = 0
+
+  let intervalDefault = 40
+  let interval = intervalDefault
+
+  const newBreak = (svg, text, height, fontSize, wordInterval) => {
+    svg.append("text")
+        .attr("x", "50%")
         .style("fill", "white")
-  
-    svg.append("rect")
-        .attr("x", 52)
-        .attr("y", 52)
-        .attr("width", params.width - 104)
-        .attr("height", params.height - 204)
-        .style("fill", "black")
-  
-    svg.append("image")
-        .attr("x", "52")
-        .attr("y", "52")
-        .attr('xlink:href', url)
-        .attr('width', "89.8%")
-        .attr('height', "73.4%")
-    
-    let heightText = 670
-    let fontSize = 300
-    let wordInterval = 0
-  
-    let intervalDefault = 40
-    let interval = intervalDefault
-  
+        .attr("text-anchor", "middle")
+        .attr("lengthAdjust", "spacingAndGlyphs")
+        .attr("y", height)
+        .attr("font-size", `${fontSize}%`)
+        .attr("textLength", wordInterval)
+        .attr("font-family", "Times New Roman") //Times New Roman
+        .text(text)
+  }
 
-    for (let i = 0; i < content.length; i++) {
-        if (i === 3) break
-  
-        const containerLength = content[i].length
-  
-        for (let i = 0; i < containerLength; i++) {
-            wordInterval += interval
-            interval--
-        }
-  
-        newBreak(svg, content[i], heightText, fontSize, wordInterval)
-        wordInterval = 0
-        interval = intervalDefault
-        heightText += 35
+  for (let i = 0; i < content.length; i++) {
+    if (i === 3) break
+    const containerLength = content[i].length
+    for (let i = 0; i < containerLength; i++) {
+      wordInterval += interval
+      interval--
     }
-    
-    const result = await convert(body.html())
-    return result
+    newBreak(svg, content[i], heightText, fontSize, wordInterval)
+    wordInterval = 0
+    interval = intervalDefault
+    heightText += 35
   }
-
-  const startProcess = async () => {
-      const urlForFetch = createURL(message.content)
-      const imageUrl = await fetchImage(urlForFetch)
-      if(!imageUrl) {
-        console.log("error")
-        return
-      }
-      return await createSvg(imageUrl)
-  }
-  return await startProcess()
 }
 
-let timeoutId = null
+const convertSvg =  (body) => {
+  let pngImage = null
+  try {
+    pngImage = convert(body.html())
+  } catch {
+    console.log("convert svg error")
+  }
+  return pngImage 
+}
+
+const createImage = async (url, message) => {
+  const d3n = new D3Node()   
+  const d3 = d3n.d3
+  const dom = new JSDOM(`<!DOCTYPE html><body></body>`)
+  const body = d3.select(dom.window.document.querySelector("body"))
+  await createSvg(body, url, message)
+  const image = convertSvg(body)
+  if(!image) console.log(image, "fail convert")
+  return image
+}
+
+const reactionOnEmoji = async (reactMessage) => {
+  if (reactMessage.emoji.reaction?._emoji.name !== params.requiredEmoji) return
+  const message = await reactMessage.message.fetch(true)
+  if (!message.content) return
+  if (message.content === "") return
+
+  const searcherUrl = createUrlFromMessage(message.content)
+  console.log("search url")
+  const imageUrl = await getUrlImage(searcherUrl)
+  if(!imageUrl) return
+  console.log("image url created")
+  if(!await validateImage(imageUrl)) return
+  console.log("image validated")
+
+  const image = await createImage(imageUrl, message.content)
+  console.log(image !== null, "image created")
+  return image  
+}
+
 let timeoutReactUsers = []
 
 client.on(Events.MessageReactionAdd, async (reaction, user) => {
-    if (reaction.message?.author?.id === 1069689657299832902) return
-    if (timeoutReactUsers.includes(user.id)) {
-      console.log("cooldown")
-      return
-    }
+  if (reaction.message?.author?.id === 1069689657299832902) return
+  if (timeoutReactUsers.includes(user.id)) {
+    console.log("cooldown")
+    return
+  }
 
-    timeoutReactUsers = [
-      ...timeoutReactUsers,
-      user.id
-    ]
+  timeoutReactUsers = [
+    ...timeoutReactUsers,
+    user.id
+  ]
 
-    timeoutId = setTimeout(() => {
-      timeoutReactUsers = [...timeoutReactUsers.filter(item => item !== user.id)]
-      timeoutId = null
-    }, 2000)
+  setTimeout(() => {
+    timeoutReactUsers = [...timeoutReactUsers.filter(item => item !== user.id)]
+  }, 2000)
 
-
-    const image = await reactionOnEmoji(reaction)
-    const channel_id =  client.channels.cache.find( channel => channel.id === (process.env.ID_CHANNEL_POST).toString())
-
-    if (!image) return
-    channel_id.send({ files: [image] });
-    dom.window.document.querySelector("body").innerHTML = ""
+  const image = await reactionOnEmoji(reaction)
+  const channel_id = client.channels.cache.find(channel => channel.id === (process.env.ID_CHANNEL_POST).toString())
+  if (!image) return
+  console.log("image ready for send")
+  channel_id.send({ files: [image] });
+  // dom.window.document.querySelector("body").innerHTML = ""
 })
 
 client.login(token)
